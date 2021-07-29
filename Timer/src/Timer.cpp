e80aa5248d1c9527c0f9920dd562d1397752c89c
SapphireAmoeba5 <67834932+SapphireAmoeba5@users.noreply.github.com>
2021-07-28 21:05:41 -0300

    Initial Commit

---
+++ Timer/src/Timer.cpp
@@ -0,0 +1,12 @@
+#include "Timer.h"
+
+#include <iostream>
+
+Timer::Timer()
+{
+}
+
+void Timer::Greet()
+{
+	std::cout << "Hello World!\n";
+}
---
+++ Timer/src/Timer.h
@@ -0,0 +1,11 @@
+#pragma once
+class Timer
+{
+public:
+	Timer();
+
+	void Greet();
+
+private:
+};
+
