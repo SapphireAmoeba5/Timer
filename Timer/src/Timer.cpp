3acef67a3bf059aee17c4ab9b5a1b475066bc376
Bob Jones <clevermail19@gmail.com>
2021-08-18 17:04:02 -0300

    Fixed Invoke

--- Timer/src/Timer.cpp
+++ Timer/src/Timer.cpp
@@ -13,7 +13,7 @@ Timer::~Timer()
 	s_ObjectCount--;
 }
 
-const void Timer::Start()
+void Timer::Start()
 {
 	i_Start();
 }
@@ -23,40 +23,7 @@ const double Timer::operator()() const
 	return GetElapsedTime();
 }
 
-inline const void Timer::i_Start()
+void Timer::i_Start()
 {
 	m_Start = std::chrono::high_resolution_clock::now();
-}
-
-
-
-namespace multithread
-{
-	size_t Timer::s_ObjectCount = 0;
-
-	Timer::Timer()
-		:m_Start(std::chrono::high_resolution_clock::now())
-	{
-
-	}
-
-	Timer::~Timer()
-	{
-		s_ObjectCount--;
-	}
-
-	void Timer::Start()
-	{
-		i_Start();
-	}
-
-	const double Timer::operator()() const
-	{
-		return GetElapsedTime();
-	}
-
-	inline const void Timer::i_Start()
-	{
-		m_Start = std::chrono::high_resolution_clock::now();
-	}
 }
--- Timer/src/Timer.h
+++ Timer/src/Timer.h
@@ -12,16 +12,20 @@ public:
 	~Timer();
 
 public: // Getters
-	inline const double GetElapsedTime() const { return std::chrono::duration<double>
-													(std::chrono::high_resolution_clock::now() - m_Start).count(); }
+	inline const double GetElapsedTime() const {
+		return std::chrono::duration<double>
+			(std::chrono::high_resolution_clock::now() - m_Start).count();
+	}
 	inline static const size_t GetObjectCount() { return s_ObjectCount; }
 
 public: // non-static Public funtions
-	const void Start();
+	void Start();
+	template<typename F, typename... A>
+	void Invoke(uint32_t ms, F&& func, A&&... args);
 
 public: // Public operator overloads
 	const double operator()() const;
-	
+
 private: // Private non-static variables
 	time_point m_Start;
 
@@ -29,56 +33,22 @@ private: // Private static variables
 	static size_t s_ObjectCount;
 
 private: // Non-static private functions
-	inline const void i_Start();
+	void i_Start();
+private: // Static private functions
+	template<typename F, typename ...A>
+	static void FuncDispatcher(uint32_t ms, F&& func, A&&... args);
 };
 
-namespace multithread
+template<typename F, typename ...A>
+void Timer::Invoke(uint32_t ms, F&& func, A&&... args)
 {
-	class Timer
-	{
-	public:
-		Timer();
-		~Timer();
+	std::thread dispatch([ms, func, args...]() {FuncDispatcher(ms, func, args...); });
+	dispatch.detach();
+}
 
-	public: // Getters
-		inline const double GetElapsedTime() const {
-			return std::chrono::duration<double>
-				(std::chrono::high_resolution_clock::now() - m_Start).count();
-		}
-		inline static const size_t GetObjectCount() { return s_ObjectCount; }
-
-	public: // non-static Public funtions
-		void Start();
-		template<typename F, typename... A>
-		void Invoke(uint32_t ms, F&& func, A... args);
-
-	public: // Public operator overloads
-		const double operator()() const;
-
-	private: // Private non-static variables
-		time_point m_Start;
-
-	private: // Private static variables
-		static size_t s_ObjectCount;
-
-	private: // Non-static private functions
-		inline const void i_Start();
-	private: // Static private functions
-		template<typename F, typename ...A>
-		static void FuncDispatcher(uint32_t ms, F&& func, A&&... args);
-	};
-
-	template<typename F, typename ...A>
-	void Timer::Invoke(uint32_t ms, F&& func, A... args)
-	{
-		std::thread dispatch([ms, func, &args...]() {FuncDispatcher(ms, func, std::forward<A>(args)...); });
-		dispatch.detach();
-	}
-
-	template<typename F, typename ...A>
-	void Timer::FuncDispatcher(uint32_t ms, F&& func, A && ...args)
-	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-		func(std::forward<A>(args)...);
-	}
+template<typename F, typename ...A>
+void Timer::FuncDispatcher(uint32_t ms, F&& func, A && ...args)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+	func(std::forward<A>(args)...);
 }
